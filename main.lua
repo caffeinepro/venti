a0f3a65141179a03d64a1addaf6da18541685fb8
Henning Hasemann <henning@leetless.de>
2012-12-01 18:02:15 +0100

    started main and world

--- main.lua
+++ main.lua
@@ -1,3 +1,34 @@
 
+local world = require 'world'
+
+
 -- main loop, die world von rechts nach links reinschiebt
+
+function love.load()
+end
+
+function love.update(dt)
+	world.update(dt)
+end
+
+function love.draw()
+end
+
+function love.mousepressed()
+end
+
+function love.mousereleased()
+end
+
+function love.keypressed()
+end
+
+function love.keyreleased()
+end
+
+function love.focus()
+end
+
+function love.quit()
+end
 
--- world.lua
+++ world.lua
@@ -1,3 +1,12 @@
 
 -- generate_objects()
 
+local M = {}
+
+function M.update(dt)
+	-- TODO: shift all objects to the left (distance depending on dt)
+	-- TODO: if necessary, generate new objects
+end
+
+return M
+
