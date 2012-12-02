1b0d0db0efda5029865005e634723d19f59dafe7
Scumpeter <git@scumpeter.de>
2012-12-02 01:06:31 +0100

    added putter function for ship.size

--- ship.lua
+++ ship.lua
@@ -7,6 +7,10 @@ function ship.lives(self)
 	return self.lives_
 end
 
+function ship.size(self)
+	return self.size
+end
+
 function ship.lives(new_lives)
 	self.lives_ = new_lives
 end
@@ -14,11 +18,11 @@ end
 -- change the position cx pixels in x- and cy pixels in y direction
 function ship.change_position(self, cx, cy)
 	-- ship can only navigate inside the viewport
-	if ((self.position_view_[X]+self.size_[X]+cx < viewport.size()[X]-1) 
+	if ((self.position_view_[X]+self.size_[X]+cx < viewport.size()[X]) 
 	and (self.position_view_[X]+cx >= 0)) then
 		self.position_view_[X]=self.position_view_[X]+(cx*self.speed_)
 	end
-	if ((self.position_view_[Y]+self.size_[Y]+cy < viewport.size()[Y]-1) 
+	if ((self.position_view_[Y]+self.size_[Y]+cy < viewport.size()[Y]) 
 	and (self.position_view_[Y]+cy >= 0)) then
 		self.position_view_[Y]=self.position_view_[Y]+(cy*self.speed_)
 	end
